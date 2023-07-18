c6b2de8f4fe1d5f0d6222037b7e3ac56ee96c60d
Vamshi <vamshi3101@gmail.com>
2023-07-18 08:50:06 +0530

    modified company types css

--- src/Components/CompanyTypes.jsx
+++ src/Components/CompanyTypes.jsx
@@ -1,35 +1,73 @@
 import React from "react";
 function CompanyTypes(){
     return (
+        <div className="bg-primary-subtle">
         <div className="container">
-            <h2 className="mt-3">Registration Criteria for OPC companies</h2>
-            <ul>
-                <li>There must be only one shareholders and only one directors.</li>
-                <li>An OPC must have only one directors and must be an Indian citizen.</li>
-                <li>There is no minimum capital requirement.</li>
-                <li>There is no continued existence allowed in One Person Company Registration.</li>
-            </ul>
-            <h2 className="mt-3">Registration Criteria for PVT LTD Company</h2>
-            <ul>
-                <li>The number of members must be between 2-200.</li>
-                <li>There must be at least two directors and two shareholders.</li>
-                <li>Each director must have a Directors Identification Number (DIN)</li>
-                <li>PAN card copy of directors/shareholders and Passport copy for NRI subscribers.</li>
-            </ul>
-            <h2 className="mt-3">Registration Criteria for LLP companies</h2>
-            <ul>
-                <li>Minimum two partners are required to incorporate an LLP.</li>
-                <li>There is no upper limit on the maximum number of partners of an LLP.</li>
-                <li>Among the partners, there should be a minimum of two designated partners who must be natural persons, and at least one of them should be resident in India.</li>
-                <li>There is no minimum capital requirement in LLP.</li>
-            </ul>
-            <h2 className="mt-3">Registration Criteria for NGO-Section 8 companies</h2>
-            <ul>
-                <li>Minimum two shareholders.</li>
-                <li>Minimum two Directors (Directors and shareholders can be same person).</li>
-                <li>At least one Director shall be resident in India.</li>
-                <li>No Minimum capital required.</li>
-            </ul>
+            <div className="container px-4 py-5">
+                <h2 className="pb-2 border-bottom border-dark">PVT LTD vs OPC vs LLP vs NGO(Section 8)</h2>
+                <p>Since adopting the 2013 Limited Liability Partnership (LLP) Act and the Companies Act, there is now more flexibility for corporate organizations. Therefore, it is critical that the entrepreneur or promoter knows the pros and cons of each business enterprise and chooses the right one (PVT, LLP, OPC, Section 8)</p>
+                <p>When a business takes the first step into the world of trade, the initial task is to register its firm. As a business owner, you are required to choose the type of company you want to set up. You have the option of starting an LLP, OPC, Partnership, Sole Proprietorship, or others.</p>
+                <p>However, as the new owner of a startup, paperwork can be overwhelming. Fortunately, the Government of India has worked towards ensuring ease of business becomes a reality. Despite this, there are a few legal hassles that new company owners can get misled by. These include </p>
+                <ul>
+                    <li>Registering themselves as private limited companies when a limited liability partnership (LLP) would suit them better.</li>
+                    <li>Whether or not it's too early, and whether or not a simple founders’ agreement would do just fine.</li>
+                </ul>
+            </div>
+            <div className="container px-4 py-5">
+                <h2 className="pb-2 border-bottom border-dark">Features of Private Limited Company</h2>
+                <p>In short, consider this legal structure by start-ups looking to raise funding or attract the best in the job market with ESOPs.</p>
+                <h3>For Businesses Raising Funding</h3>
+                <p>Fast-growing businesses that will require funding from venture capitalists (VCs) need to register as private limited companies. This is because only private limited companies can make them shareholders and offer them a board of directors. LLPs would require investors to be partners, and OPCs cannot accommodate additional shareholders. Therefore, if you're raising funding, the points that follow scarcely matter; your decision is made.</p>
+                <h3>Requires Greater Compliance</h3>
+                <p>In exchange for the convenience of easily accommodating funding, the private limited company set-up needs to meet the Ministry of Corporate Affairs (MCA). These range from a statutory audit, annual filings with the Registrar of Companies (RoC), annual submission of IT returns, as well as quarterly board meetings, the filing of minutes of these meetings, and much else. Suppose your business isn't geared to meet these requirements. In that case, you may want to wait a while before you jump into registering a private limited company (some businesses first register an LLP because the compliances are fewer).</p>
+                <h3>Few Tax Advantages</h3>
+                <p>The private limited company is assumed to have many tax advantages, but this is not the case. There are some industry-specific advantages, but taxes are to be paid at a flat rate of 30% on profits, the dividend distribution tax (DDT) applies, as does Minimum Alternate Tax (MAT). If you're looking for the structure with the lowest tax burden, the LLP does offer some better benefits.</p>
+                <h3>Start-up Cost</h3>
+                <p>A private limited company costs around Rs. 8000 to start at the very least, excluding professional fees. However, this will be higher in some states; in Kerala, Punjab and Madhya Pradesh, the fees are much higher. It would help if you also had some paid-up capital, which can be as little as Rs. 5000 to begin with. The annual compliance costs are around Rs. 13,000.</p>
+            </div>
+            <div className="container px-4 py-5">
+                <h2 className="pb-2 border-bottom border-dark">Features of an LLP</h2>
+                <p>The LLP is meant for professional and advisory firms with no need for equity funding. If this applies to your business, pick the LLP; it's been gaining in popularity since 2008 because it combines some of the better aspects of partnership firm and private limited company.</p>
+                <h3>For Non-Scalable Businesses</h3>
+                <p>If you're running a business that's unlikely to require equity funding, you may want to register an LLP as it combines several benefits of the private limited company and general partnership. Like a private limited company, it has limited liability and has a simpler structure, like a general partnership.</p>
+                <h3>Fewer Compliances</h3>
+                <p>The MCA has made some concessions to the LLP. For example, an audit needs to be performed only if your turnover is greater than Rs. 40 lakh or paid-up capital is more than Rs. 25 lakh. Furthermore, whereas all structural changes need to be communicated to the RoC in the case of private limited companies, the requirement is minimal for LLPs.</p>
+                <h3>Tax Advantages</h3>
+                <p>If your business is earning over Rs. 1 crore in profits, the LLP offers tax benefits. The tax surcharge applies to companies with profits over Rs. 1 crore doesn't apply to LLPs, nor does Dividend Distribution Tax. Loans to partners are also not taxable as income.</p>
+                <h3>Number of Partners</h3>
+                <p>There is no limit to the number of partners in an LLP. So if you're building a large advertising agency, for example, you needn't worry about any cap on the number of partners.</p>
+                <h3>Startup Cost</h3>
+                <p>Much cheaper than starting a private limited company, with government fees of Rs. 5000, no paid-up capital and low compliance costs.</p>
+            </div>
+            <div className="container px-4 py-5">
+                <h2 className="pb-2 border-bottom border-dark">Features of an OPC</h2>
+                <p>An OPC is not much different from a private limited company, except that there is only one director (although there must be a nominee), who will also be the sole shareholder.</p>
+                <h3>For Solo Entrepreneurs</h3>
+                <p>A significant improvement over the sole proprietorship firm, given that your liability is limited, the OPC is meant for solo entrepreneurs. However, note that if it has over Rs. 2 crore revenues, and paid-up capital of over Rs. 50 lakh, it needs to be converted into a private limited company. Furthermore, given that there must be a nominee director (to enable the perpetual existence of the OPC), you may also consider starting a private limited company, which will also have the flexibility of raising funding.</p>
+                <h3>High Compliance Requirements</h3>
+                <p>While there are no board meetings, you have to conduct a statutory audit, submit annual and IT returns, and comply with the various requirements of the MCA.</p>
+                <h3>Minimal Tax Advantages</h3>
+                <p>The OPC, like the private limited company, has some industry-specific advantages. But taxes are to be paid at a flat rate of 30% on profits, the DDT applies, as does MAT. If you're looking for a structure with the lowest tax burden, the LLP does offer some better benefits.</p>
+                <h3>Start-up Costs</h3>
+                <p>Nearly the same as a private limited company, with government fees of a little less than Rs. 7,000. However, this will change for different states; in Kerala, Punjab and Madhya Pradesh, the fees are much higher.</p>
+            </div>
+            <div className="container px-4 py-5">
+                <h2 className="pb-2 border-bottom border-dark">Features of Section 8 Company</h2>
+                <p>Not all companies today aim to make huge profits or even profits by carrying out the normal process of trade or commerce. Several companies have objectives of being charitable and generating zero profit. Search companies are known as Section 8 companies and are recognized under Section 8 of the Companies Act 2013. These companies will likely dedicate all the income or profit towards charitable objectives. The Companies Act defines a Sec 8 company as a company whose primary objective is to encourage the field of commerce, art, education, research, science, sports, social welfare, charity environment, legal protection, or other similar goals. These companies donate all the profit towards the objective and do not pay any dividends to the members. Previously section 25 of Companies in 1956 defined all these objectives with fewer or more provisions. However, the described new act has better goals than Section 8 companies can make the most of. Features of Section 8 Company are listed below.</p>
+                <h3>Charitable objectives</h3>
+                <p>Section 8 companies do not have any motive to make profits. The objective here is to be entirely charitable. They support the causes, including cultural science, religion, sports, etc.</p>
+                <h3>No Minimum Share Capital</h3>
+                <p>Unlike other general companies, They do not have any prescribed paid-up capital minimum limit.</p>
+                <h3>Limited Liability</h3>
+                <p>Members of Section 8 companies have only limited liability. In any case, the penalties cannot be unlimited.</p>
+                <h3>Government Companies</h3>
+                <p>They can only work if they have a licence from the central government. In some cases, the government also has the right to revoke consent.</p>
+                <h3>Privileges</h3>
+                <p>As the companies have charitable objectives, the Companies Act offers several benefits and exemptions to them.</p>
+                <h3>Companies As Members</h3>
+                <p>Besides individuals and the association of people, Section 8 also allows the companies to be members of these companies.</p>
+            </div>
+        </div>
         </div>
     )
 }
